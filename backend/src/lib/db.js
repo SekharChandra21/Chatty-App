b8a08d78b26eb9edb3c67de669d13005ad2a935b
CHANDRASEKHAR ARIPAKA <106866978+SekharChandra21@users.noreply.github.com>
2025-06-30 00:40:16 +0530

    Update db.js

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 export const connectDB = async () =>{
     try {
-        const conn = await mongoose.connect("mongodb+srv://sekhararipaka456:Hn7jtJRwecHVoe6f@cluster0.j8um9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+        const conn = await mongoose.connect("your_database_url");
         console.log(`MongoDB connected: ${conn.connection.host}`);
     } catch (error) {
         console.error(`Error: ${error.message}`);
